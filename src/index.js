89367f9de5fa1ae16d881325421b9398de002574
Vanessa Lopes <80737823+VanessaLops@users.noreply.github.com>
2023-01-03 21:00:17 -0300

    querys iniciais apollo server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('iiii');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: async () => {
+        return 'Hello again';
+      },
+      hi: () => {
+        return 'Hi';
+      },
+    },
+  },
+});
+
+// const PORT = 4005;
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Iniciou na porta ${url}`);
+});
