0776f7fe0c684e5f79ec62f9f604797e3e24c19d
VanessaLops <vanessashirlene_estrela@hotmail.com>
2023-02-24 22:31:23 -0300

    deletando codigo comentádo

--- src/graphql/context.js
+++ src/graphql/context.js
@@ -15,21 +15,3 @@ export const context = () => {
     getPosts: _getPosts,
   };
 };
-
-// import fetch from 'node-fetch';
-// import { getPosts } from './post/utils';
-// import { getUsers } from './user/utils';
-// import { makeUserDataLoader } from './user/dataloaders';
-// import { makePostDataLoader } from './post/datasources';
-
-// const _getUsers = getUsers(fetch);
-// const _getPosts = getPosts(fetch);
-
-// export const context = () => {
-//   return {
-//     userDataLoader: makeUserDataLoader(_getUsers),
-//     postDataLoader: makePostDataLoader(_getPosts),
-//     getUsers: _getUsers,
-//     getPosts: _getPosts,
-//   };
-// };
